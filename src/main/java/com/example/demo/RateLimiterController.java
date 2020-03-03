e8a7b2e28067980c1eebbaca3d6592fec3ba422e
cdelabs <ctc.cheatz@gmail.com>
2020-03-03 14:34:25 -0600

    Should be good?

--- src/main/java/com/example/demo/RateLimiterController.java
+++ src/main/java/com/example/demo/RateLimiterController.java
@@ -1,5 +1,47 @@
 package com.example.demo;
 
-public class RateLimiterController {
+import java.time.Duration;
+import java.time.LocalTime;
+import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import io.github.resilience4j.ratelimiter.RateLimiter;
+import io.github.resilience4j.ratelimiter.RateLimiterConfig;
+import io.github.resilience4j.ratelimiter.RateLimiterRegistry;
+
+public class RateLimiterController {
+	private final AtomicLong counter = new AtomicLong();
+	private RateLimiter rateLimiter;
+	public static String str1;
+
+	@GetMapping("/rl")
+	public String callService(@RequestParam(value = "val", defaultValue = "30") String val) {
+		// Wrap service call in ratelimiter & call service.
+		Runnable runnable = () -> callService();
+		rateLimiter.executeRunnable(runnable);
+		return str1;
+	}
+
+	@Bean
+	public void LowRateServiceCallerClient() {
+		/*
+		 * Create rate limiter to allow 5 calls every 5 seconds & keep other calls
+		 * waiting until maximum of 10 seconds.
+		 */
+		RateLimiterConfig config = RateLimiterConfig.custom().limitRefreshPeriod(Duration.ofMillis(5000))
+				.limitForPeriod(101).timeoutDuration(Duration.ofMillis(30000)).build();
+
+		// Create registry
+		RateLimiterRegistry rateLimiterRegistry = RateLimiterRegistry.of(config);
+
+		// Use registry
+		rateLimiter = rateLimiterRegistry.rateLimiter("externalConcurrentService");
+	}
+
+	public void callService() {
+		str1 = LocalTime.now() + " Call processing finished in Rate Limiter, call number: " + counter.incrementAndGet();
+	}
 }
