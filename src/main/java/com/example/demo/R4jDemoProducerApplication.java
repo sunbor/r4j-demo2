a25ee143d51627d780b51b9fa4b515840a762e84
cdelabs <sunbor@oregonstate.edu>
2020-03-03 10:23:17 -0600

    added test path

--- src/main/java/com/example/demo/R4jDemoProducerApplication.java
+++ src/main/java/com/example/demo/R4jDemoProducerApplication.java
@@ -2,10 +2,11 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class R4jDemoProducerApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(R4jDemoProducerApplication.class, args);
 	}
